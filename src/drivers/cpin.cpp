be044573d4535233eef724a92ad3e11eb60d266a
Dmitry Dvorkin <dvorkin@tibbo.com>
2016-07-01 13:44:13 +0300

    cpin.init can change pin now

--- src/drivers/cpin.cpp
+++ src/drivers/cpin.cpp
@@ -2,7 +2,7 @@
 
 #include "global.h"
 
-FILE *CPin::open_x( int _pin, const char *_what) {
+FILE *CPin::x_open( int _pin, const char *_what) {
  FILE *fp;
  char fpath[ PATH_MAX];
  sprintf( fpath, SYS_GPIO_PFX"gpio%d/%s", _pin, _what);
@@ -12,6 +12,9 @@ FILE *CPin::open_x( int _pin, const char *_what) {
 int CPin::init( int _pin) {
  DIR *dp;
  char fpath[ PATH_MAX];
+ if ( this->pin != _pin) {
+   this->x_close();  this->pin = 0;
+ }
  if ( this->fp_rw) return( 0);
  sprintf( fpath, SYS_GPIO_PFX"gpio%d", _pin);
  if ( !( dp = opendir( fpath))) {
@@ -25,7 +28,7 @@ int CPin::init( int _pin) {
 
 int CPin::R( void) {
  char ret;
- if ( !this->fp_rw) this->fp_rw = this->open_x( this->pin, "value");
+ if ( !this->fp_rw) this->fp_rw = this->x_open( this->pin, "value");
  if ( !this->fp_rw) return( -1);
  fflush( this->fp_rw);
  fseek( this->fp_rw, 0L, SEEK_SET);
@@ -35,7 +38,7 @@ int CPin::R( void) {
 int CPin::W( int _val) {
  int ret;
  char x[ 2];
- if ( !this->fp_rw) this->fp_rw = this->open_x( this->pin, "value");
+ if ( !this->fp_rw) this->fp_rw = this->x_open( this->pin, "value");
  if ( !this->fp_rw) return( -1);
  x[ 1] = '\0';
  x[ 0] = ( _val > 0 ? '1' : '0');
@@ -46,7 +49,7 @@ int CPin::W( int _val) {
 
 int CPin::dir_get( void) {
  static char ss[ PATH_MAX];
- FILE *fp = this->open_x( this->pin, "direction");
+ FILE *fp = this->x_open( this->pin, "direction");
  if ( !fp) return( -1);
  int ret = fscanf( fp, "%1s", ss);
  fclose( fp);
@@ -56,7 +59,7 @@ int CPin::dir_get( void) {
 int CPin::dir_set( int _dir) {
  const char *ss = "in";
  int r = 0;
- FILE *fp = this->open_x( this->pin, "direction");
+ FILE *fp = this->x_open( this->pin, "direction");
  if ( !fp) return( -1);
  if ( _dir == PIN_DIR_O) ss = "out";
  r = fwrite( ss, sizeof( char), strlen( ss), fp);
