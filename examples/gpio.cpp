b8d08fec4b3b4510623610d50e0dde23974cd2ec
Vitaliy Gribko <vitaliy.gribko@tibbo.com>
2016-07-01 18:21:43 +0300

    Update gpio example

--- examples/gpio.cpp
+++ examples/gpio.cpp
@@ -3,14 +3,14 @@
 */
 
 #include "ltps/capi/gpio.h"
-
+#include "ltps/capi/cpin.h" // Defines
 
 int main()
 {
     Gpio gpio;
 
-    if (gpio.getDirection("S1A") == input)
-        gpio.setDirection("S1A", output);
+    if (gpio.getDirection("S1A") == PIN_DIR_I)
+        gpio.setDirection("S1A", PIN_DIR_O);
 
     if (gpio.getValue("S1A") == 0)
         gpio.setValue("S1A", 1);
