24731b82c2bf24cd14e6d027992d70568262102a
Vitaliy Gribko <vitaliy.gribko@tibbo.com>
2016-07-01 10:11:49 +0300

    Small fix

--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,7 @@
 # define UNUSED(x) x
 #endif
 
-#include "drivers/gpio.h"
-
 int main(int UNUSED(argc), char *UNUSED(argv[]))
 {
-    Gpio gpio;
 
-    if (gpio.getDirection("S1A") == input)
-        gpio.setDirection("S1A", output);
-
-    if (gpio.getValue("S1A") == 0)
-        gpio.setValue("S1A", 1);
 }
