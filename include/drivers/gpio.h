82efee2c3c789ba17b80aab4908d4216e1fbb25b
Vitaliy Gribko <vitaliy.gribko@tibbo.com>
2016-08-18 17:10:00 +0300

    Add GPIO setDirection/setValue function for pin groupe

--- include/drivers/gpio.h
+++ include/drivers/gpio.h
@@ -11,6 +11,20 @@
     \brief Class for GPIO manipulations
 */
 
+/*!
+    Bit arithmetic
+
+    1. To disable n bit in data
+    data &= ~(1 << n)
+    2.To enable n bit in data
+    data |= (1 << n)
+    3. To toogle n bit in data
+    data ^= (1 << n)
+    4. To get n nit in data
+    (data >> n) & 1
+*/
+
+
 class Gpio
 {
 public:
@@ -27,6 +41,15 @@ public:
     */
     int setDirection(const char *pin, int direction);
 
+    /// Set directions for the pin group simultaneously (see bit arithmetic)
+    /*!
+        \param lsbPins Pins S1A...S15D (LSB bit 0 - S1A, bit 1 - S1B,.., MSB bit 31 - S15D)
+        \param msbPins Pins S17A..S25C (LSB bit 0 - S17A, bit 1 - S17B,..., MSB bit 18 - S25D)
+        \param lsbDirs Directions for pins S1A...S15D (0 for input, 1 for output)
+        \param msbDirs Directions for pins S17A...S25D (0 for input, 1 for output)
+    */
+    void setDirection(int lsbPins, int msbPins, int lsbDirs, int msbDirs);
+
     /// Get direction
     /*!
         \param pin String name of pin (eg: S1A, S9D)
@@ -41,6 +64,15 @@ public:
         \return Result (< 0 on error)
     */
     int setValue(const char* pin, unsigned int value);
+
+    /// Set values for the pin group simultaneously (see bit arithmetic)
+    /*!
+        \param lsbPins Pins S1A...S15D (LSB bit 0 - S1A, bit 1 - S1B,.., MSB bit 31 - S15D)
+        \param msbPins Pins S17A..S25C (LSB bit 0 - S17A, bit 1 - S17B,..., MSB bit 18 - S25D)
+        \param lsbValues Directions for pins S1A...S15D (0 for input, 1 for output)
+        \param msbValues Directions for pins S17A...S25D (0 for input, 1 for output)
+    */
+    void setValue(int lsbPins, int msbPins, int lsbValues, int msbValues);
 
     /// Get value
     /*!
--- src/drivers/gpio.cpp
+++ src/drivers/gpio.cpp
@@ -10,6 +10,63 @@
 
 #include "gpio.h"
 
+namespace GpioPrivate
+{
+    const char* lsb[] =
+    {
+        "S1A",  "S1B",  "S1C",  "S1D",
+        "S3A",  "S3B",  "S3C",  "S3D",
+        "S5A",  "S5B",  "S5C",  "S5D",
+        "S7A",  "S7B",  "S7C",  "S7D",
+        "S9A",  "S9B",  "S9C",  "S9D",
+        "S11A", "S11B", "S11C", "S11D",
+        "S13A", "S13B", "S13C", "S13D",
+        "S15A", "S15B", "S15C", "S15D"
+    };
+
+    const char* msb[] =
+    {
+        "S17A", "S17B", "S17C", "S17D",
+        "S19A", "S19B", "S19C", "S19D",
+        "S21A", "S21B", "S21C", "S21D",
+        "S23A", "S23B", "S23C", "S23D",
+        "S25A", "S25B", "S25C"
+    };
+
+    inline void setData(int lsbPins, int msbPins, int lsbData, int msbData, bool direction)
+    {
+        for (unsigned int n = 0; n < sizeof(GpioPrivate::lsb) / sizeof(const char*); n++)
+        {
+            if ((lsbPins >> n) & 1)
+            {
+                char *error;
+                CPin* cpin = Lutils::getInstance().getGpioPointer(GpioPrivate::lsb[n], &error);
+
+                if (cpin)
+                {
+                    if (direction) cpin->dir_set((lsbData >> n) & 1);
+                    else cpin->W((lsbData >> n) & 1);
+                }
+            }
+        }
+
+        for (unsigned int n = 0; n < sizeof(GpioPrivate::msb) / sizeof(const char*); n++)
+        {
+            if ((msbPins >> n) & 1)
+            {
+                char *error;
+                CPin* cpin = Lutils::getInstance().getGpioPointer(GpioPrivate::msb[n], &error);
+
+                if (cpin)
+                {
+                    if (direction) cpin->dir_set((msbData >> n) & 1);
+                    else cpin->W((msbData >> n) & 1);
+                }
+            }
+        }
+    }
+}
+
 Gpio::Gpio()
 {
 
@@ -31,6 +88,11 @@ int Gpio::setDirection(const char *pin, int direction)
         printf("GPIO set direction error for socket %s - %s\n", pin, error);
 
     return -1;
+}
+
+void Gpio::setDirection(int lsbPins, int msbPins, int lsbDirs, int msbDirs)
+{
+    GpioPrivate::setData(lsbPins, msbPins, lsbDirs, msbDirs, true);
 }
 
 int Gpio::getDirection(const char *pin)
@@ -57,6 +119,11 @@ int Gpio::setValue(const char* pin, unsigned int value)
         printf("GPIO set direction error for socket %s - %s\n", pin, error);
 
     return -1;
+}
+
+void Gpio::setValue(int lsbPins, int msbPins, int lsbValues, int msbValues)
+{
+    GpioPrivate::setData(lsbPins, msbPins, lsbValues, msbValues, false);
 }
 
 unsigned int Gpio::getValue(const char *pin)
